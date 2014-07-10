84083a117ffe599adb4887d7d159f1e9b63caceb
Daniel Boorn <info@rapiddigitalllc.com>
2014-07-10 13:43:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
 
 	
 	# Example of \F1\Exception
-	/*
+	
 	try{
 		$f1 = \F1\API::forge();
 	}catch(\F1\Exception $e){
-		var_dump($e);
+		var_dump($e, $e->getMessage(), $e->getReponse());
 	}
-	*/
+	
 	
 	
 	try{
@@ -38,12 +38,11 @@
 		$f1 = \F1\API::forge();//2nd party auto sign-in
 		
 		# Show available chain paths by id
-		//var_dump($f1->paths);
+		var_dump($f1->paths);
 		
 		
 
 		# example of household search with array return
-		/*
 		$r = $f1->households()->search(array(
 			'searchFor' => 'Boorn',
 			'include'=> 'address,communications,attributes',
@@ -53,32 +52,29 @@
 			echo $r['results']['household'][0]['householdName'];
 		}
 		var_dump($r);
-		*/		
+				
 		
 		
 		# example of house hold search with phpQuery xml doc return
-		/*
 		$doc = $f1->xml()->households()->search(array(
 			'searchFor' => 'Boorn',
 		 	'include'=> 'address,communications,attributes',
 		))->get();
 		echo $doc['householdName']->text();
-		var_dump($doc);
-		*/
+		echo $doc;
 		
 		//to switch back to json use: $f1->json() on your next chain request
 		 
 		# search people (note the json)
-		/*
+		
 		$r = $f1->json()->people()->search(array( 
 			'searchFor' => 'Boorn',
 			'include'=> 'address,communications,attributes',		
 		))->get();
 		var_dump($r);
-		*/
+		
 		
 		# search people (xml with selectors)
-		/*
 		$doc = $f1->xml()->people()->search(array(
 			'searchFor' => 'Boorn',
 			'include' => 'address,communications,attributes',
@@ -86,35 +82,33 @@
 		echo $doc['person']->attr('id');
 		echo $doc['person:first maritalStatus']->text();
 		echo $doc['person:first communication:first communicationValue']->text();
-		*/
+		
 		
 		# edit person (array or ojbect)
-		/*
+		
 		$personId = 12121222;
 		$model = $f1->json()->people($personId)->edit()->get();
 		$model['person']['firstName'] = 'Daniel';
 		$model = $f1->people($model['person']['@id'])->update($model);
-		*/
+		
 		
 		# edit person (xml)
-		/*
+		
 		$personId = 12121222;
 		$model = $f1->xml()->people($personId)->edit()->get();
 		$model['person']['firstName']->text('Daniel');
-		$model = $f1->people($model['person']->attr('id'))->update((string)$model);
-		*/
+		$model = $f1->people($model['person']->attr('id'))->update($model)->get();
+		
 		
 		// ... any path defined in the api_paths.js file is chainable ...
 		
-		/*
 		$r = $f1->funds()->list()->get();
 		var_dump($r);
-		*/
 		
-		/*
+		
 		$r = $f1->events()->list()->get();
 		var_dump($r);
-		*/
+		
 		
 		
 		//build your own chain to an api method...
